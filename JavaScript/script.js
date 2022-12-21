93c9b41eaadf29b8eb6dc61532f3fb35aa691307
Ery MouraTech <erimilson1998@gmail.com>
2022-12-21 12:18:52 -0300

    inicio

---
+++ JavaScript/script.js
@@ -0,0 +1,13 @@
+function menu()
+{
+    var nav = document.getElementById("links");
+
+    if(nav.style.display === "block")
+    {
+        nav.style.display = "none";
+    }
+    else
+    {
+        nav.style.display = "block";
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Home-Page
-Moura Tech Criando uma Home Page Responsiva HTML5, CSS3
+# Home Page
+ 
+#
+
+### Moura Tech Criando uma Home Page Responsiva HTML5, CSS3
+
+
+
+
+
+<div>
+    <a href="#HTML">GIT</a>
+</div>
